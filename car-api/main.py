d7b5a0e1fe0eff823d39d96c46c45267378dd7f5
Petar Stoychev <p.stoychev@student.fontys.nl>
2022-05-22 23:04:43 +0200

    prepare for docker

--- car-api/main.py
+++ car-api/main.py
@@ -154,4 +154,4 @@ def uploadimg():
     return "200"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(debug=False, host="0.0.0.0", port=5001)
--- frontend/main.py
+++ frontend/main.py
@@ -54,4 +54,4 @@ def car(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0")
