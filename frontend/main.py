6b02998dcf2423f902c784022d55b909ba124b87
Ryan <ryan.smith@fontys.nl>
2022-06-04 15:17:40 +0200

    frontend

--- frontend/main.py
+++ frontend/main.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import json
-from flask import Flask, render_template, send_file, send_from_directory
+from flask import Flask, request, render_template, send_file, send_from_directory
 
 app = Flask(__name__)
 
@@ -20,7 +20,29 @@ def root():
     names = content["names"]
     ids = content["ids"]
 
-    return render_template("index.html", brands=names, ids=ids)
+    if "brand" in request.args:
+        brand = request.args["brand"]
+        year = request.args["year"] if "year" in request.args else None
+        km = request.args["km"] if "km" in request.args else None
+
+        carlist = json.loads(requests.get(f"{API_ENDPOINT}/carlist/{brand}").content)
+        found=[]
+        for car in carlist:
+            add=False
+            if not year == None:
+                if car["year"] <= int(year):
+                    add=True
+        
+            if not km == None:
+                if car["km"] <= int(km):
+                    add=True
+                else:
+                    add=False
+            if add:
+                found.append(car)
+
+
+    return render_template("index.html", brands=names, ids=ids, cars=found)
 
 def findNameFromId(id, ids, names):
     for x in range(len(ids)):
