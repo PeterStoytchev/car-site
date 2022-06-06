064e433d7ee6063c5a3e5ce5844fe351e0fb2551
Petar Stoychev <p.stoychev@student.fontys.nl>
2022-06-06 18:44:01 +0200

    fix deployment issues

--- build-mysql-pod.sh
+++ build-mysql-pod.sh
@@ -3,7 +3,7 @@ CURRENT_POS="600"
 
 start_pod_stmt="CHANGE MASTER TO MASTER_HOST='94.fontyslab.com',MASTER_PORT=21401,MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
 start_pod_cmd='export MYSQL_PWD=111; mysql -u root -e "'
-start_pod_cmd+="$start_slave_stmt"
+start_pod_cmd+="$start_pod_stmt"
 start_pod_cmd+='"'
 
 sudo docker exec mysql_pod sh -c "$start_pod_cmd"
