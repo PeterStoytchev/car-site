3dae5b6f1c18b02cb4c90ba278c1d4007d55eab1
Rahimi,Edris E <e.rahimi@student.fontys.nl>
2022-06-12 21:36:49 +0200

    update to the tkinter app code

--- car tkinter/main.py
+++ car tkinter/main.py
@@ -8,7 +8,7 @@ API_ENDPOINT = f"http://{ENDP}"
 
 # Basic tkinter configuration
 root = Tk()
-root.title("Add car AD")
+root.title("Edit car AD")
 root.geometry("600x450")
 
 # Input boxes & label
@@ -34,7 +34,7 @@ km_label.place(x=20, y=125)
 
 modell = Entry(root, width=50)
 modell.place(x=160, y=150)
-modell_label = Label(root, text="Model(<1 - 24>):")
+modell_label = Label(root, text="Model(<1>):")
 modell_label.place(x=20, y=150)
 
 price = Entry(root, width=50)
@@ -75,9 +75,12 @@ def btn_clicked():
             "price":pr,
             "year":ye
             }
-    header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    requests.post(f"{API_ENDPOINT}/edit/{mo}",data=json.dumps(items), headers=header)
-
+            
+    header = {'Content-type': 'application/json'}
+    print(items)
+    # get /models/<brand_id> for all available models
+    resp = requests.post(f"{API_ENDPOINT}/edit/{mo}",data=json.dumps(items), headers=header)
+    print(resp)
 
 # BTN
 btn = Button(root, text="Upload to the website!", command=btn_clicked)
